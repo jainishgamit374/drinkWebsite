139ebfb330905828860935937abff4fe2fc1b4fb
xvsanuj <anuj.shar2k8@gmail.com>
2024-06-26 08:15:55 +0530

    Marque Fix

--- src/Components/Marque.jsx
+++ src/Components/Marque.jsx
@@ -3,53 +3,47 @@ const Marque = () => {
 
   return (
     <div className="marque h-[30vh] w-full flex flex-nowrap">
-    
-    {
-      arr.map((item, index)=>{
-        console.log(item)
-      })
-    }
-
-      {/* <div className="h-[30vh] flex items-center gap-6 flex-shrink-0 w-full bg-[#004D43] text-white relative -m-px">
-        <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-        <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-        <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-        <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-      </div>
-      <div className="h-[30vh] flex items-center gap-6 flex-shrink-0 w-full bg-[#004D43] text-white relative -m-px">
-      <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-        <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-        <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-        <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-      </div>
-      <div className="h-[30vh] flex items-center gap-6 flex-shrink-0 w-full bg-[#004D43] text-white relative -m-px">
-      <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-        <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-        <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-        <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-      </div>
-      <div className="h-[30vh] flex items-center gap-6 flex-shrink-0 w-full bg-[#004D43] text-white relative -m-px">
-      <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-        <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-        <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-        <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">REDBULL</h1>
-        <img className="h-28 object-contain" src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png" alt="" />
-      </div> */}
+      {arr.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="h-[30vh] flex items-center gap-6 flex-shrink-0 w-full bg-[#004D43] text-white relative -m-px"
+          >
+            <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">
+              REDBULL
+            </h1>
+            <img
+              className="h-28 object-contain"
+              src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png"
+              alt=""
+            />
+            <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">
+              REDBULL
+            </h1>
+            <img
+              className="h-28 object-contain"
+              src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png"
+              alt=""
+            />
+            <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">
+              REDBULL
+            </h1>
+            <img
+              className="h-28 object-contain"
+              src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png"
+              alt=""
+            />
+            <h1 className="font-[Kajiro] kajiro text-[10vw] font-semibold">
+              REDBULL
+            </h1>
+            <img
+              className="h-28 object-contain"
+              src="https://em-content.zobj.net/source/apple/391/high-voltage_26a1.png"
+              alt=""
+            />
+          </div>
+        );
+      })}
     </div>
   );
 };
