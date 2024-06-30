9ef0e6afffb15e7a9c1169c3a9db93a7d20d6c34
xvsanuj <anuj.shar2k8@gmail.com>
2024-06-30 21:51:25 +0530

    Color Commit

---
+++ src/Components/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='min-h-screen w-full'>
+        <div className='h-60 w-full border-y-[1px] border-black'>
+            <h1 className='monument'>Ammy Virk</h1>
+        </div>
+        <div className='h-60 w-full border-y-[1px] border-black'></div>
+        <div className='h-60 w-full border-y-[1px] border-black'></div>
+        <div className='h-60 w-full border-y-[1px] border-black'></div>
+    </div>
+  )
+}
+
+export default About
