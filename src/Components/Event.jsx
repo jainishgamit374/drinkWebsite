cc7bea029ab7016493d351af66ce0e2d815bec28
xvsanuj <anuj.shar2k8@gmail.com>
2024-07-01 16:22:07 +0530

    Events Section Pre

---
+++ src/Components/Event.jsx
@@ -0,0 +1,19 @@
+const Event = ({data}) => {
+  var {image_background, image_upper, date, footer_heading, title} = data;
+  return (
+    <div className="h-[90%] relative select-none cursor-pointer w-72 flex-shrink-0 drop-shadow-2xl rounded-2xl bg-white overflow-hidden">
+      <img className="h-[70%] opacity-35 w-full relative object-cover" src={image_background} alt=""/>
+      <span className="h-6 w-6 bg-white rounded-full absolute bottom-[27%] -left-[3%]"></span>
+      <span className="h-6 w-6 bg-white rounded-full absolute bottom-[27%] -right-[3%]"></span>
+      <div className="px-5 py-2">
+        <h1 className="text-lg tracking-tighter font-bold">{title}</h1>
+        <p className="text-sm font-semibold">{date}</p>
+        <p className="font-bold py-5">{footer_heading}</p>
+      </div>
+      <div className="h-[70%] w-full absolute top-0 left-0">
+        <img className="absolute h-28 w-full top-1/2 left-1/2 -translate-x-2/4 -translate-y-2/4 object-contain" src={image_upper} alt="" />
+      </div>
+    </div>
+  );
+};
+export default Event;
