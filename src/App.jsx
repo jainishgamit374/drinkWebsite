f5329f496dcfaeff77176741dfeec8f1a6145a28
jainishgamit374 <jainishgamit374@gmail.com>
2024-07-03 21:52:07 +0530

    refactor: Add Lenis scroll animation to App component

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,28 @@ import React from "react";
 import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
 import Marque from "./Components/Marque";
-import LocomotiveScroll from "locomotive-scroll";
 import Footer from "./Components/Footer";
 import Athletes from "./Components/Athletes";
 import Discover from "./Components/Discover";
 import Partners from "./Components/Partners";
 import Ingredients from "./Components/Ingredients";
 import Events from "./Components/Events";
+import Lenis from "lenis";
 
 const App = () => {
-  const locomotiveScroll = new LocomotiveScroll();
+  function lenisFn() {
+    const lenis = new Lenis({
+      wheelMultiplier: 2,
+      duration: 1.5,
+      easing: (x) => 1 - Math.pow(1 - x, 5),
+    });
+    function raf(time) {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    }
+    requestAnimationFrame(raf);
+  }
+  lenisFn();
   return (
     <div className="h-full w-full overflow-hidden">
       <Navbar/>
